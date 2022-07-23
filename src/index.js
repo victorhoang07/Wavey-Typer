d00c69369f28f8ca9b5a8982fb36b5016a3da55c
Victor Hoang <vikterhoang@gmail.com>
2022-07-23 18:18:55 -0400

    added some waves

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import Game from "./scripts/game"
-
+import Wave from "./scripts/waves_view"
+// import * as dat from 'dat.gui';
 
 document.addEventListener("DOMContentLoaded", function() {
-    let game = new Game
+    const game = new Game
     game.startGame()
+    
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d')
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    let waves = new Wave(canvas, ctx, game)
+    waves.draw1(ctx)
+    waves.draw2(ctx)
+
+
+    function animate () {
+    requestAnimationFrame(animate)
+    waves.draw1(ctx)
+    waves.draw2(ctx)
+    }
+
+    animate()
+
 
     window.addEventListener('keydown', function (e) {
     document.querySelector('p').innerHTML = `You pressed ${e.key}`;
     }, false);
+
+    
 })
---
+++ src/scripts/waves_view.js
@@ -0,0 +1,51 @@
+import * as dat from 'dat.gui';
+
+export default class Wave {
+
+    constructor(canvas, ctx, game) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.game = game;
+        this.width = canvas.width
+        this.height = canvas.height
+        this.gui = new dat.GUI()
+        this.wave1 = {
+            y: this.height,
+            length: 0.01,
+            amplitude: 100,
+            frequency: 0.01
+        }   
+        this.frequency = this.wave1.frequency
+        // this.wave2 = {
+        //     y: this.height/2,
+        //     length: 0.01,
+        //     amplitude: 100
+        // }   
+        this.gui.add(this.wave1, 'y', 0, this.height)
+        this.gui.add(this.wave1, 'length', -0.01, 0.10)
+        this.gui.add(this.wave1, 'amplitude', -300, 300)
+        this.gui.add(this.wave1, 'frequency', -0.01, 1 )
+    }
+
+    draw1 (ctx) {
+        ctx.clearRect(0,0, this.width, this.height)
+        ctx.beginPath()
+        ctx.moveTo(0, this.height / 4)
+        for (let i = 1; i < this.width; i++) {
+            ctx.lineTo(i, this.wave1.y / 4 + Math.sin(i * this.wave1.length - this.frequency) * this.wave1.amplitude)
+        }
+        ctx.stroke()
+    }
+
+    draw2 (ctx) {
+        // ctx.clearRect(0,0, this.width, this.height)
+        ctx.beginPath()
+        ctx.moveTo(0, this.height / 4 )
+        for (let i = 1; i < this.width; i++) {
+            ctx.lineTo(i,this.wave1.y /1.3 + Math.sin(i * this.wave1.length + this.frequency) * this.wave1.amplitude)
+        }
+        ctx.stroke()
+        this.frequency += this.wave1.frequency
+    }
+
+}
